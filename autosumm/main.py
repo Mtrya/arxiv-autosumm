caabc94d7ee5f5efe7871623e4ba82abfe454eb0
Mtrya <erchanmion20@outlook.com>
2025-09-30 09:28:50 +0800

    fix: Fix typo in main

--- autosumm/main.py
+++ autosumm/main.py
@@ -259,7 +259,7 @@ def setup_logging(log_dir: str, send_log: bool, verbose: bool = False):
     # Suppress INFO/WARNING logs from noisy libraries
     logging.getLogger('arxiv').setLevel(logging.ERROR)
     logging.getLogger('pdfminer').setLevel(logging.ERROR)
-    logging.getLogger('urllib3').setLevel(logging.Error)
+    logging.getLogger('urllib3').setLevel(logging.ERROR)
     
     return logging.getLogger(__name__), log_file_path
 
